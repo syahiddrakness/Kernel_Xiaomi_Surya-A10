3c43d5d15980cdfb64e913b74184a9ede0442ba0
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-28 23:10:29 +0200

    init: Add CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
    * Ignoring an ignore flag, yikes
    * Also replace skip_initramf with want_initramf (omitting last letter for Magisk since it binary patches that out of kernel, I'm not even sure why we're supporting that mess)
    
    Co-authored-by: Erfan Abdi <erfangplus@gmail.com>
    Change-Id: Ifdf726f128bc66bf860bbb71024f94f56879710f

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,6 +3,29 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+#include <asm/setup.h>
+#endif
+
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+#define INITRAMFS_STR_FIND "skip_initramf"
+#define INITRAMFS_STR_REPLACE "want_initramf"
+#define INITRAMFS_STR_LEN (sizeof(INITRAMFS_STR_FIND) - 1)
+
+static char proc_command_line[COMMAND_LINE_SIZE];
+
+static void proc_command_line_init(void) {
+	char *offset_addr;
+
+	strcpy(proc_command_line, saved_command_line);
+
+	offset_addr = strstr(proc_command_line, INITRAMFS_STR_FIND);
+	if (!offset_addr)
+		return;
+
+	memcpy(offset_addr, INITRAMFS_STR_REPLACE, INITRAMFS_STR_LEN);
+}
+#endif
 
 #ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
 extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
@@ -16,8 +39,14 @@ static int cmdline_proc_show(struct seq_file *m, void *v)
 		return 0;
 	}
 #endif
+
 	seq_puts(m, saved_command_line);
 	seq_putc(m, '\n');
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+	seq_printf(m, "%s\n", proc_command_line);
+#else
+	seq_printf(m, "%s\n", saved_command_line);
+#endif
 	return 0;
 }
 
@@ -40,6 +69,10 @@ static int __init proc_cmdline_init(void)
 	offset_addr = strstr(saved_command_line, "androidboot.mode=reboot");
 	if (offset_addr != NULL)
 		strncpy(offset_addr + 17, "normal", 6);
+
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+	proc_command_line_init();
+#endif
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
