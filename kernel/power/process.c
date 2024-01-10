c7ee89d8acbd5df75b93652e48ae85e29269d44c
syahiddraakness <syahidselaluspd708@gmail.com>
2024-01-10 19:03:04 +0700

    qcom: Setting PM QOS Signed-off-by: Syahid <syahidselaluspd708@gmail.com>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -28,7 +28,7 @@
 /*
  * Timeout for stopping processes
  */
-unsigned int __read_mostly freeze_timeout_msecs = 40 * MSEC_PER_SEC;
+unsigned int __read_mostly freeze_timeout_msecs = 60 * MSEC_PER_SEC;
 
 static int try_to_freeze_tasks(bool user_only)
 {
@@ -84,11 +84,11 @@ static int try_to_freeze_tasks(bool user_only)
 		/*
 		 * We need to retry, but first give the freezing tasks some
 		 * time to enter the refrigerator.  Start with an initial
-		 * 10 ms sleep followed by exponential backoff until 15 ms.
+		 * 15 ms sleep followed by exponential backoff until 20 ms.
 		 */
-		usleep_range(sleep_usecs / 10, sleep_usecs);
-		if (sleep_usecs < 15 * USEC_PER_MSEC)
-			sleep_usecs *= 10;
+		usleep_range(sleep_usecs / 15, sleep_usecs);
+		if (sleep_usecs < 20 * USEC_PER_MSEC)
+			sleep_usecs *= 15;
 	}
 
 	end = ktime_get_boottime();
